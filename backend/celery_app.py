69c9c0d7372e3f1ccd606bb8d9dcadc54912bee2
Denys Kibalenko <dennieking87@gmail.com>
2026-04-22 11:37:09 +0200

    Chore: create project's core structure

---
+++ backend/celery_app.py
@@ -0,0 +1,11 @@
+# backend/celery_app.py
+import os
+
+from celery import Celery
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+
+app = Celery("spa_comments")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
