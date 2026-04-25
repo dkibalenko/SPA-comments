ab8f19c9b521c9f6a3d6deb9f39ac2b859233702
Denys Kibalenko <dennieking87@gmail.com>
2026-04-25 16:04:05 +0200

    Feat: add url configuration with DefaultRouter for CommentViewSet in backend/apps/comments/urls.py & setup backend.config/urls.py accrodingly

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/", include("apps.comments.urls")),
 ] + debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
