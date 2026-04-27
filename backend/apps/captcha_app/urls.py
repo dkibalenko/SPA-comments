758d01cadba7913c317397b9902c089de4ad5ea8
Denys Kibalenko <dennieking87@gmail.com>
2026-04-27 17:25:20 +0200

    Feat: map CaptchaGenerateView to captcha/ url path in backend/apps/captcha_app/urls.py & include to backend/config/urls.py

---
+++ backend/apps/captcha_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from apps.captcha_app.views import CaptchaGenerateView
+
+
+urlpatterns = [
+    path("captcha/", CaptchaGenerateView.as_view(), name="captcha-generate"),
+]
